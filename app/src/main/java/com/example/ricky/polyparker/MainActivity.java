a50699ff2cb3e44c9abc2d95761c05130209effa
raturcios <pcscricky@hotmail.com>
2015-04-15 08:07:58 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/ricky/polyparker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ricky.polyparker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+// Comment for test
+/**
+ * Created by Ricky on 4/14/2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
