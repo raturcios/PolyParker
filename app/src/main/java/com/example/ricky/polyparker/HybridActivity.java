12390f9d80a2bf6b6d0a5706c808b45254ad9b32
raturcios <pcscricky@hotmail.com>
2015-04-22 16:43:46 -0700

    Hybrid Push

---
+++ app/src/main/java/com/example/ricky/polyparker/HybridActivity.java
@@ -0,0 +1,16 @@
+package com.example.ricky.polyparker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ricky on 4/21/2015.
+ */
+public class HybridActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hybrid);
+    }
+}
